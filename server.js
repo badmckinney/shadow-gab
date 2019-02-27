1b620938f89c06385d882392d19911ddef3a26b8
Brad McKinney <brad.mckin@gmail.com>
2019-02-27 07:37:48 -1000

    remove console logs and reformat

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-//loads in libraries
+//load in libraries
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const socketIO = require('socket.io')({
-  transports: [                    
+  transports: [
     'websocket'
-]
+  ]
 });
 const http = require('http');
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-//loads in local imports
-const {mongoose} = require('./server/db/mongoose');
-const {User} = require('./server/models/user');
-const {generateMessage, generateLocationMessage} = require('./server/utils/message');
-const {isRealString} = require('./server/utils/validation');
-const {Users} = require('./server/utils/users');
+//load in local imports
+const { mongoose } = require('./server/db/mongoose');
+const { User } = require('./server/models/user');
+const { generateMessage, generateLocationMessage } = require('./server/utils/message');
+const { isRealString } = require('./server/utils/validation');
+const { Users } = require('./server/utils/users');
 
 const port = process.env.PORT || 3000;
 
-//creates a new express app
+//create a new express app
 const app = express();
 
 //middleware config
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
-  secret:'OJ did it',
+  secret: 'OJ did it',
   saveUninitialized: true,
   resave: true
 }));
@@ -44,30 +44,24 @@ const users = new Users();
 
 app.get('/', (req, res) => {
   if (req.session.screenname) {
-    console.log(req.session);
     res.redirect('/chat');
   } else {
-    console.log(req.session);
     res.sendFile(path.join(__dirname + '/public/index.html'));
   }
 });
 
-//Moved this middleware after GET '/' request to give my request higher priority
 app.use(express.static(__dirname + '/public'));
 
 app.get('/signup', (req, res) => {
   if (req.session.screenname) {
-    console.log(req.session);
     res.redirect('/chat');
   } else {
-    console.log(req.session);
     res.sendFile(path.join(__dirname + '/public/signup.html'));
   }
 });
 
 app.get('/chat', (req, res) => {
   if (!req.session.screenname) {
-    console.log(req.session);
     res.redirect('/');
   }
 
@@ -81,12 +75,10 @@ app.get('/logout', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  console.log(req.body);
   User.findOne({ screenname: req.body.screenname }, (err, doc) => {
     if (err) {
       res.status(500).send('error occured');
     } else if (doc) {
-      console.log(doc)
       res.status(500).send('Screenname already taken');
     } else {
       let user = new User({
@@ -104,50 +96,45 @@ app.post('/users', (req, res) => {
 });
 
 app.post('/chat', (req, res) => {
-    console.log(req.body);
 
-    User.findOne({ screenname: req.body.screenname }, (err,doc) => {
-      if (err) {
-        res.status(500).send('error occured');
-      } else if (!doc) {
-        res.status(500).send('Account doesn\'t exist. Please sign up.');
-      } else if (doc) {
-        console.log(doc);
-        let valid = bcrypt.compareSync(req.body.password, doc.password);
+  User.findOne({ screenname: req.body.screenname }, (err, doc) => {
+    if (err) {
+      res.status(500).send('error occured');
+    } else if (!doc) {
+      res.status(500).send('Account doesn\'t exist. Please sign up.');
+    } else if (doc) {
+      let valid = bcrypt.compareSync(req.body.password, doc.password);
 
-        if (!valid) {
-          res.status(500).send('Incorrect Password');
-        } else if (valid) {
-          if (!req.session.screenname) {
-            res.sendFile(path.join(__dirname + '/public/chat.html'));
-            io.on('connection', (socket) => {
-              console.log('New user connected');
+      if (!valid) {
+        res.status(500).send('Incorrect Password');
+      } else if (valid) {
+        if (!req.session.screenname) {
+          res.sendFile(path.join(__dirname + '/public/chat.html'));
+          io.on('connection', (socket) => {
 
-              socket.on('join', (params, callback) => {
-                if (!isRealString(req.body.screenname) || !isRealString(req.body.room)) {
-                  return callback('Name and room name are required');
-                }
+            socket.on('join', (params, callback) => {
+              if (!isRealString(req.body.screenname) || !isRealString(req.body.room)) {
+                return callback('Name and room name are required');
+              }
 
-                socket.join(req.body.room);
-                users.removeUser(socket.id);
-                users.addUser(socket.id, req.body.screenname, req.body.room);
+              socket.join(req.body.room);
+              users.removeUser(socket.id);
+              users.addUser(socket.id, req.body.screenname, req.body.room);
 
-                io.to(req.body.room).emit('updateUserList', users.fetchUserList(req.body.room));
-                console.log(req.session.screenname);
-                if (!req.session.screenname) {
-                  socket.emit('newMessage', generateMessage('Admin', `Welcome to ${req.body.room} room`));
-                  socket.broadcast.to(req.body.room).emit('newMessage', generateMessage('Admin', `${req.body.screenname} has joined`));
-                }
-                req.session.screenname = req.body.screenname;
-                console.log(req.session.screenname);  
-                callback();
+              io.to(req.body.room).emit('updateUserList', users.fetchUserList(req.body.room));
+              if (!req.session.screenname) {
+                socket.emit('newMessage', generateMessage('Admin', `Welcome to ${req.body.room} room`));
+                socket.broadcast.to(req.body.room).emit('newMessage', generateMessage('Admin', `${req.body.screenname} has joined`));
+              }
+              req.session.screenname = req.body.screenname;
+              callback();
 
-              });
             });
-          }
+          });
         }
       }
-    });
+    }
+  });
 });
 
 /*---------------------------
